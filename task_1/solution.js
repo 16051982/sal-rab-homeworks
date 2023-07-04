9d117decbaf1534bfc6db2b7deb7665fb3c77992
Anna <novoros2015@inbox.ru>
2023-07-04 11:07:52 +0300

    12vb

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,7 +3,7 @@ function addInCartMessage(title, price) {
     let productPrice = price;
     let x = "теперь в корзине";
     let message = productName;
-    console.log(message + x);
+    console.log(message + productPrice + x);
 
     
     // Задание №1.1. Оповещение о добавлении в корзину
