1665a4466775aa8a7dd32d6e1286e31607159666
Anna <novoros2015@inbox.ru>
2023-08-14 12:17:43 +0300

    0888

--- task_final/solution.js
+++ task_final/solution.js
@@ -35,6 +35,7 @@
 
 function sendRequest(name, phone, address, goods, sum) {
     let data = {goods: [], order: {}};
+    let address = {street, house, floor, flat, entrance};
 
     let countOfGoods = goods.length;
 
@@ -43,10 +44,10 @@ function sendRequest(name, phone, address, goods, sum) {
     }
 
     data.order.address = address;
+    data.order.goods = title + countOfGoods;
     data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
+    
+    
     let jsonData = JSON.stringify(data);
 
     return jsonData;
